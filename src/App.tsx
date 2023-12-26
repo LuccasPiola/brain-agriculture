b132f0a54993ebd8295b7d2cea9fe1af2b24577d
Luccas Piola <lhc.piola@gmail.com>
2023-12-25 21:04:37 -0300

    feat: add main application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <div />;
-}
+import { useEffect } from 'react';
 
-export default App;
+import Box from '@mui/material/Box';
+
+import { fetchFarmersThunk } from '@/store/farmers/actions';
+import { useAppDispatch } from '@/store/hooks';
+import Menu from '@/components/Menu';
+
+import RouteHandler from './routes';
+
+export default function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFarmersThunk());
+  }, [dispatch]);
+
+  return (
+    <Box className="flex">
+      <Menu />
+      <RouteHandler />
+    </Box>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import React from 'react';
+
+import { ThemeProvider } from '@mui/material/styles';
+
+import theme from './lib/mui-theme.ts';
 import App from './App.tsx';
+import store from './store';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
